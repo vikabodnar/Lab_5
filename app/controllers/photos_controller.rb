d3cec63cc7c93dbfee4c4309d6794cc92d907925
victoria <vbodnar99@gmail.com>
2017-11-27 23:02:54 +0200

    Gallery_fixed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,33 +1,35 @@
 class PhotosController < ApplicationController
-  def index
-    pp @photos = Photo.all
+  before_action :authenticate_user!
 
+  def index
+    @photos = Photo.all
   end
 
   def new
-    @photo = Photo.new 
-  end
-  def create
+    @photo = Photo.new
+   end
+
+   def create
     params[:photo][:category_id] = params[:category_id]
-    @photo = current_user.photos.build(photos_params) 
+    params[:photo][:user_id] = current_user.id
+    @photo = Photo.create(photo_params)
     redirect_to photos_path
-  end
-  
-  def edit
-    
-  end
+   end
 
-  def update
-    
-  end
+   def edit
+   end
 
-  def destroy
-    
-  end
+ 
 
-  private
-def photos_params
-  params.require(:photo).permit(:user_id, :category_id, :name, :photo)
-end
+   def update
+   end
 
+   def destroy
+   end
+
+private
+
+   def photo_params
+    params.require(:photo).permit(:user_id, :category_id, :name, :photo)
+   end
 end
